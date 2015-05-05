d67ddb701160f7c58a376ba6d707282f489dd058
Jan Filipowski <jachuf@gmail.com>
2015-05-05 11:29:02 +0200

    DomainExtractor gem introduced

---
+++ README.md
@@ -0,0 +1,29 @@
+# DomainExtractor
+
+## Installation
+
+Add this line to your application's Gemfile:
+
+```ruby
+gem 'domain_extractor'
+```
+
+And then execute:
+
+    $ bundle
+
+Or install it yourself as:
+
+    $ gem install domain_extractor
+
+## Usage
+
+TODO: Write usage instructions here
+
+## Contributing
+
+1. Fork it ( https://github.com/[my-github-username]/domain_extractor/fork )
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create a new Pull Request
---
+++ lib/domain_extractor.rb
@@ -0,0 +1,75 @@
+require "singleton"
+require "addressable/uri"
+
+class CantExtractDomain < StandardError
+end
+
+class DomainTLD
+  def initialize(tld)
+    @tld = tld
+    @suffix = ".#{@tld}"
+  end
+
+  def matches?(domain)
+    domain.end_with?(@suffix) && domain != @tld
+  end
+end
+
+class DomainExtractor
+  include Singleton
+
+  def initialize
+    source_file = File.join("data", "effective_tld_names.dat")
+    @known_domain_suffixes = []
+    File.open(source_file, "r") do |file|
+      @known_domain_suffixes = file.each_line.map do |line|
+        DomainTLD.new(line.strip)
+      end
+    end
+  end
+
+  def extract(url)
+    fail_if_empty(url)
+
+    host = fetch_host(url)
+
+    fail_if_empty(host)
+
+    domain = clear_hostname(host)
+
+    fail_if_empty(domain)
+
+    fail_if_incorrect_tld(domain)
+
+    domain
+  rescue URI::InvalidURIError
+    raise CantExtractDomain.new
+  end
+
+
+  private
+    def fail_if_empty(string)
+      raise CantExtractDomain.new if string.nil? || string.empty?
+    end
+
+    def fetch_host(url)
+      if url =~ /http(s)?:\/\//
+        uri = Addressable::URI.parse(clear_url(url))
+        uri.host
+      else
+        clear_url(url)
+      end
+    end
+
+    def clear_hostname(host)
+      domain = host.gsub(/^www\./, "")
+    end
+
+    def fail_if_incorrect_tld(domain)
+      raise CantExtractDomain.new if @known_domain_suffixes.find{|x| x.matches?(domain)}.nil?
+    end
+
+    def clear_url(url)
+      url.downcase.gsub(/http(:)+\/(\/)+/, "http://").gsub(/https(:)+\/(\/)+/, "https://").gsub(/\/$/, '')
+    end
+end
